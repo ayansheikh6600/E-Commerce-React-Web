bf1a8b633016ade7067752b7f38544b11a1f4293
ayansheikh6600 <ayansheikh6600@gmail.com>
2023-09-24 13:34:38 +0500

    bottom navbar complete

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -3,33 +3,33 @@ import { NavLink } from 'react-router-dom'
 
 const BottomNavbar = () => {
   return (
-    <ul className='justify-between py-2 px-4 flex  sm:hidden sticky bottom-0 bg-white'>
-        <li>
+    <ul className='justify-between px-4 flex h-14 items-center sm:hidden sticky bottom-0 bg-[#fc8937] text-white'>
+        <li className='h-full flex items-center'>
             <NavLink to="/" className={({isActive})=>{
-                        return isActive? "w-full border-b-2 border-[#fc8937]" : ""
+                        return isActive? "h-[80%] mb-3  rounded-b-3xl flex items-center w-12 bg-white text-[#fc8937]" : "h-full flex items-center w-10 "
                     }}>
-                <i className="fa-solid fa-house"></i>
+                <i className="fa-solid fa-house mx-auto"></i>
             </NavLink>
         </li>
-        <li>
+        <li className='h-full flex items-center'> 
         <NavLink to="/about" className={({isActive})=>{
-                        return isActive? "w-full border-b-2 border-[#fc8937]" : ""
+                        return isActive? "h-[80%] mb-3  rounded-b-3xl flex items-center w-12 bg-white text-[#fc8937]": "h-full flex items-center w-10 "
                     }}>
-                <i className="fa-solid fa-circle-info"></i>
+                <i className="fa-solid fa-circle-info mx-auto"></i>
             </NavLink>
         </li>
-        <li>
+        <li className='h-full flex items-center'>
         <NavLink to="/contact" className={({isActive})=>{
-                        return isActive? "w-full border-b-2 border-[#fc8937]" : ""
+                        return isActive? "h-[80%] mb-3  rounded-b-3xl flex items-center w-12 bg-white text-[#fc8937]" : "h-full flex items-center w-10 "
                     }}>
-                <i className="fa-solid fa-phone"></i>
+                <i className="fa-solid fa-phone mx-auto"></i>
             </NavLink>
         </li>
-        <li className='border-2 border-black'>
+        <li className='h-full flex items-center'>
         <NavLink to="/products" className={({isActive})=>{
-                        return isActive? "w-full border-b-2 border-[#fc8937]" : "p-3"
+                        return isActive? "h-[80%] mb-3  rounded-b-3xl flex items-center w-12 bg-white text-[#fc8937]" : "h-full flex items-center w-10 "
                     }}>
-                <i className="fa-solid fa-cart-shopping"></i>
+                <i className="fa-solid fa-cart-shopping mx-auto"></i>
             </NavLink>
         </li>
     </ul>
